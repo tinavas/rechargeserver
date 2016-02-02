1dd5b47cd617018bf7ad944e028c65444126d0e7
rashidacse <rashida57pust@gmail.com>
2016-02-02 21:22:23 +0600

    wating loader

---
+++ application/views/wating_loader.php
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+    $(window).load(function () {
+        $(".loader").fadeOut("slow");
+    });
+</script>
+<style type="text/css">
+    .loader {
+        position: fixed;
+        left: 0px;
+        top: 0px;
+        width: 100%;
+        height: 100%;
+        z-index: 9999;
+        background: url('<?php echo base_url(); ?>resources/images/loading.gif') 50% 50% no-repeat rgb(249,249,249);
+    }
+
+</style>
