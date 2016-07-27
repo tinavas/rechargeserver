84e35778a2a1da4624136f0bf54ace3502a476ea
nazmulhasan <nazmul@apurbatech.com>
2016-07-27 23:34:00 +0600

    Pin view page

---
+++ application/views/reseller/pin.php
@@ -0,0 +1,17 @@
+<div class="ez-asterisk-holder">
+    <div id="ez-wrap" class="ez-asterisk-holder ez-main-wrap ez-auth-pages ez-login-wrap">
+        <div class="ez-page-title ">
+            <h1>Pin</h1>
+            <?php echo $message;?>
+        </div>
+        <div id="ez-login-wrap">
+            <?php echo form_open("auth/pin");?>
+                <div class="ez-form-row" style="min-height: 100px">
+                    <label for="pin">Pin</label>
+                    <input id="pin" name="pin" class="ez-ltr" type="text" autocomplete="off">
+                </div>
+                <input type="submit" value="Submit" class="ez-full-width">
+            <?php echo form_close();?>
+        </div>
+    </div>
+</div>
