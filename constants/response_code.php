01c5b7b811e8bfebd7ea6ed7aa6c487c18b66f69
rashidacse <rashida57pust@gmail.com>
2016-08-14 09:37:16 +0600

    bug fix

--- constants/response_code.php
+++ constants/response_code.php
@@ -1,10 +1,11 @@
 <?php
 define("RESPONSE_CODE_SUCCESS",                                 2000);
-define("ERROR_CODE_INVALID_AMOUNT",                             5001);
+define("ERROR_CODE_INVALID_AMOUNT",                             5000);
 define("ERROR_CODE_INVALID_MEMBER_COUNTER",                     5002);
 define("ERROR_CODE_SUBSCRIPTION_PERIOD_EXPIRED",                5003);
 define("ERROR_CODE_MAXINUM_MEMBERS_CREATION_REACHED",           5004);
 define("ERROR_CODE_DB_SQL_EXCEPTION",                           5005);
 define("ERROR_CODE_DB_SETUP_EXCEPTION",                         5006);
 define("ERROR_CODE_SERVER_EXCEPTION",                           5007);
+define("ERROR_CODE_SESSION_EXPIRED",                            5001);
 
