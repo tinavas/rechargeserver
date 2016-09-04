18e96d10363013491a4f38160d6d5a7b4872b596
rashidacse <rashida57pust@gmail.com>
2016-09-04 16:48:13 +0600

    pagination library  and superadmin  tamplete

---
+++ constants/admin_templates.php
@@ -0,0 +1,3 @@
+<?php 
+define('SUPER_ADMIN_LOGIN_TMPL',                 'superadmin/non_member/templates/main_tmpl');
+define('SUPER_ADMIN_TEMPLATE',                   'superadmin/templates/main_tmpl');
