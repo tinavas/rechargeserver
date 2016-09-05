394a64f47c21c85f98f0a6318424ab182b36167a
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-09-05 19:46:31 +0600

    footer height related bug fixing

--- application/views/nonmember/templates/sections/footer.php
+++ application/views/nonmember/templates/sections/footer.php
@@ -1,22 +1,7 @@
-<footer>
-    <div class="ez-snippet" id="ez-snippet-sharedpagefooter">
-        <div class="ez-wrap">
-            <div class="ez-snippet" id="ez-snippet-footersecuritylogos">
-                <ul id="ez-footer-cards">
-                    <li class="ez-paypal"></li>
-                    <li class="ez-mcard"></li>
-                    <li class="ez-visa"></li>
-                    <li class="ez-payments">Accepted payments methods</li>
-                </ul>
-
-                <ul id="ez-footer-security">
-                    <li>Powered By - <?php echo SITE_TITLE;?></li>
-                </ul>
-
-                <div class="clear"></div>
-            </div>
-            <div class="clear"></div>
+<footer> 
+    <div class="row">
+        <div class="col-sm-12 footer_text">
+            Powered By - <?php echo SITE_TITLE; ?>
         </div>
     </div>
-    <div class="ez-clear-both"></div>
 </footer>
