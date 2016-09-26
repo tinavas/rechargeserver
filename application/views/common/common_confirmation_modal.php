8ced5b04a930d0e798b0e703182381632b5220d7
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-09-26 16:21:29 +0600

    logical update of topup confitmation modal

--- application/views/common/common_confirmation_modal.php
+++ application/views/common/common_confirmation_modal.php
@@ -8,7 +8,7 @@
                     </div>
                 </div>
             </div>
-            <div class="modal-body" style="text-align: center;">
+            <div class="modal-body" style="text-align: center; font-size: 16px;">
                 <div class="row form-group">
                     <div class="col-md-12">
                         <div id="confirmation_content" class="common_confirmation_modal_content"></div>
@@ -17,8 +17,11 @@
             </div>
             <div class="modal-footer">
                 <div class="row">
-                    <div class="col-md-offset-5 col-md-3">
+                    <div class="col-md-offset-2 col-md-3">
                         <input id="modal_confirm_click_id" type="button" class="btn button-custom btn_custom_button" data-dismiss="modal" aria-hidden="true" value="Confirm">
+                    </div>
+                    <div class="col-md-3">
+                        <input id="modal_edit_click_id" type="button" class="btn button-custom btn_custom_button" data-dismiss="modal" aria-hidden="true" value="Edit">
                     </div>
                     <div class="col-md-3">
                         <input id="modal_cancel_click_id" type="button" class="btn custom_cancel_btn btn_custom_button close" data-dismiss="modal" aria-hidden="true" value="Cancel">
