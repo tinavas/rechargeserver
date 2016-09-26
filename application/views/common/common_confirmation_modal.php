8a8a6557907a3f9dbc2d58528b013faa7a4e2b65
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-09-26 13:30:13 +0600

    common confirmation modal added

---
+++ application/views/common/common_confirmation_modal.php
@@ -0,0 +1,30 @@
+<div class="modal fade" id="common_confirmation_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content modal_background_color">
+            <div class="modal-header">
+                <div class="row">
+                    <div class="col-md-12">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-body" style="text-align: center;">
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <div id="confirmation_content" class="common_confirmation_modal_content"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <div class="row">
+                    <div class="col-md-offset-5 col-md-3">
+                        <input id="modal_confirm_click_id" type="button" class="btn button-custom btn_custom_button" data-dismiss="modal" aria-hidden="true" value="Confirm">
+                    </div>
+                    <div class="col-md-3">
+                        <input id="modal_cancel_click_id" type="button" class="btn custom_cancel_btn btn_custom_button close" data-dismiss="modal" aria-hidden="true" value="Cancel">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
