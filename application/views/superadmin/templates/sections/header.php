1da6e9cc928fc8f2b3070b6a4de2b8061a0dad67
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-10-03 18:13:49 +0600

    bug fixing

--- application/views/superadmin/templates/sections/header.php
+++ application/views/superadmin/templates/sections/header.php
@@ -1,5 +1,5 @@
 <div style="background-color: #0072C6;">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row" style="padding: 15px;">
             <div class="col-md-12" >
                 <a  class="admin_header_text_style" href="<?php echo base_url() ?>superadmin/auth">Admin panel</a> 
